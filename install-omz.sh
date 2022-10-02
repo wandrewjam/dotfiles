e3b7a555326f0c3227cc23ff8cd88c6155e2ad2c
Andrew Watson <Andrew.Watson@sas.com>
2022-10-02 08:44:18 -0400

    Add script to install omz

---
+++ install-omz.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+if [ -f "$HOME/.oh-my-zsh"] 
+then
+    omz update
+else
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
