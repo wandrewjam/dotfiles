5572cba696a7ec16c4d20260a49caa3b1b003e17
Andrew Watson <Andrew.Watson@sas.com>
2022-10-02 10:44:00 -0400

    Fix another bug

--- install-omz.sh
+++ install-omz.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-if [ -f "$HOME/.oh-my-zsh" ] 
+if [ ! -d "$HOME/.oh-my-zsh" ] 
 then
-    omz update
-else
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
